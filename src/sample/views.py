1f8739a211a7699741c5b9a09293c4800c88a251
Kevin Teague <kevin@bud.ca>
2016-04-06 21:41:22 -0700

    display installed libraries

--- src/sample/views.py
+++ src/sample/views.py
@@ -1,7 +1,14 @@
 from pyramid.view import view_config
+import subprocess
 
 @view_config(route_name='hello', renderer='templates/hello.pt')
 def request_reset_view(request):
     """Request password reset form"""
-    return {}
+    
+    output = subprocess.check_output('pip list', shell=True)
+    output = output.decode('ascii')
+    output = output.split('\n')
+    output = [lib for lib in output if (not lib.startswith('sample-app') and lib)]
+    
+    return {'libs':output}
 
